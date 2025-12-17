3bc2ea3c68e226621ea55717604b7e5d4d82f20e
Bhargava Naidu <bhargava@bootlabstech.com>
2025-12-17 11:44:29 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,68 +1,15 @@
-resource "google_compute_instance" "default" {
-  name         = var.name
-  machine_type = var.machine_type
-  zone         = var.zone
-  project      = var.project
-
-  boot_disk {
-    initialize_params {
-      size  = var.boot_disk_size
-      type  = var.boot_disk_type
-      image = var.boot_disk_image
-    }
-    kms_key_self_link = var.kms_key_self_link == "" ? null : var.kms_key_self_link
-  }
-
-	// Allow the instance to be stopped by terraform when updating configuration
-  //allow_stopping_for_update = var.allow_stopping_for_update
-
-  metadata_startup_script = var.enable_startup_script ? templatefile("${path.module}/startup.sh", {}) : null
-
-  metadata = {
-    enable-oslogin = "TRUE"
-  }
-  network_interface {
-    subnetwork = var.subnetwork
-
-    # dynamic access_config {
-    #   for_each = var.address_type == "EXTERNAL" ? [{}] : (var.address == "" ? [] : [{}])
-
-    #   content {
-    #     nat_ip = var.address_type == "EXTERNAL" ? google_compute_address.static[0].address : (var.address == "" ? null : google_compute_address.static[0].address)
-    #   }
-    # }
-  }
+resource "google_artifact_registry_repository" "artifact-repo" {
+  repository_id = var.name_of_repo 
+  provider      = google-beta
+  project       = var.project_id
+  location      = var.location
+  kms_key_name  = var.kms_key_name
+  format        = var.format
+  mode          = var.mode
+   docker_config {
+    immutable_tags = var.format == "DOCKER" ? true : false
+  } 
   lifecycle {
-    ignore_changes = [boot_disk, attached_disk, labels, metadata, service_account, tags]
+    ignore_changes = [ cleanup_policies ]
   }
-	service_account {    
-      email = var.sa_email
-      scopes = var.service_account_scopes
-  }
-
-  shielded_instance_config {
-    enable_secure_boot          = true
-    enable_integrity_monitoring = true
-  }
-
-  # timeouts {
-  #   create = "10m"
-  # }
 }
-
-# resource "google_service_account" "default" {
-# 	count = "${var.create_service_account ? 1 : 0}"
-#   account_id   = var.sa_email
-#   display_name = format("%s Compute Instance", var.name)
-#   project      = var.project
-# }
-
-# resource "google_compute_address" "static" {
-#   count         = var.address_type == "INTERNAL" ? (var.address == "" ? 0 : 1) : 1
-#   name          = format("%s-external-ip", var.name)
-#   project       = var.compute_address_project
-#   region        = var.compute_address_region
-#   address_type  = var.address_type
-#   subnetwork    = var.subnetwork
-#   address       = var.address_type == "INTERNAL" ? (var.address == "" ? null : var.address) : null	
-# }
--- variables.tf
+++ variables.tf
@@ -1,109 +1,26 @@
-// required variables
-variable "name" {
+variable "project_id" {
   type        = string
-  description = "A unique name for the resource, required by GCE. Changing this forces a new resource to be created."
+  description = "Name of the project"
 }
-
-variable "machine_type" {
+variable "location" {
   type        = string
-  description = "The machine type to create."
+  description = "Name of the location this repository is located in."
 }
 
-variable "compute_address_region" {
+variable "name_of_repo" {
   type        = string
-  description = "The region that the compute address should be created in. If it is not provided, the provider zone is used."
+  description = "Name of the repository id in the artifact registry"
 }
-
-variable "compute_address_project" {
+variable "kms_key_name" {
   type        = string
-  description = "The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
+  description = "Name of the kms key artifact_registry_repository"
 }
-// optional variables
-
-variable "zone" {
+variable "format" {
   type        = string
-  description = "The zone that the machine should be created in. If it is not provided, the provider zone is used."
+  description = "The format of packages that are stored in the repository.eg Docker Image Manifest V2, Schema 1, Docker Image Manifest V2, Schema 2,Open Container Initiative (OCI)"
 }
-
-variable "project" {
+variable "mode" {
   type        = string
-  description = "The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
-}
-
-variable "tags" {
-  type        = list(string)
-  description = "A list of network tags to attach to the instance."
-  default     = []
-}
-
-variable "network" {
-  type        = string
-  description = " The name or self_link of the network to attach this interface to. Either network or subnetwork must be provided. If network isn't provided it will be inferred from the subnetwork."
-}
-
-variable "subnetwork" {
-  type        = string
-  description = "The name or self_link of the subnetwork to attach this interface to. Either network or subnetwork must be provided. If network isn't provided it will be inferred from the subnetwork. The subnetwork must exist in the same region this instance will be created in. If the network resource is in legacy mode, do not specify this field. If the network is in auto subnet mode, specifying the subnetwork is optional. If the network is in custom subnet mode, specifying the subnetwork is required."
-}
-
-variable "boot_disk_image" {
-  description = "The image from which to initialize this disk. This can be one of: the image's self_link, projects/{project}/global/images/{image}, projects/{project}/global/images/family/{family}, global/images/{image}, global/images/family/{family}, family/{family}, {project}/{family}, {project}/{image}, {family}, or {image}. If referred by family, the images names must include the family name. If they don't, use the google_compute_image data source. For instance, the image centos-6-v20180104 includes its family name centos-6. These images can be referred by family name here."
-  type        = string
-}
-
-variable "boot_disk_size" {
-  description = "The size of the image in gigabytes. If not specified, it will inherit the size of its base image."
-  type        = string
-}
-
-variable "boot_disk_type" {
-  description = "The GCE disk type. May be set to pd-standard, pd-balanced or pd-ssd."
-  type        = string
-}
-
-variable "enable_startup_script" {
-  type        = bool
-  description = "Enable startup script, include startup.sh"
-  default     = false
-}
-
-variable "create_service_account" {
-  type        = bool
-  description = "Create service account for the compute instance"
-  default     = false
-}
-
-variable "service_account_scopes" {
-  type        = list(string)
-  description = "A list of service scopes. Both OAuth2 URLs and gcloud short names are supported. To allow full access to all Cloud APIs, use the cloud-platform scope."
-  default     = ["cloud-platform"]
-}
-
-variable "allow_stopping_for_update" {
-  type        = bool
-  description = "If true, allows Terraform to stop the instance to update its properties. If you try to update a property that requires stopping the instance without setting this field, the update will fail."
-  default     = false
-}
-
-variable "kms_key_self_link" {
-  type        = string
-  description = "The self_link of the encryption key that is stored in Google Cloud KMS to encrypt this disk."
-  default     = ""
-}
-
-variable "address_type" {
-  type        = string
-  description = "The type of address to reserve. Default value is EXTERNAL. Possible values are INTERNAL and EXTERNAL"
-}
-
-variable "address" {
-  type        = string
-  description = "The private ip of the compute-instance"
-  default      = ""
-}
-
-variable "sa_email" {
-  type        = string
-  description = "The service account to attach to the aws instance"
-  default     = ""
+  description = "The mode of the repository.Possible values are: STANDARD_REPOSITORY, VIRTUAL_REPOSITORY, REMOTE_REPOSITORY."
+  default     = "STANDARD_REPOSITORY"
 }
